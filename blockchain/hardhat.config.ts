2c1afd2aa282a644238b9236fabb6bdc70d82cde
BerkantB <berkant.bakisli@gmail.com>
2022-08-22 14:00:01 +0300

    Blockchain: Update ledger contract

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import '@nomiclabs/hardhat-ethers'
 
+import * as dotenv from "dotenv";
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [
+        process.env.PRIVATE_KEY!,
+      ],
+    }
+  }
 };
 
 export default config;
